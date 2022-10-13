17eed4a96379ea999a695efe4e791f149f5aa709
juetEm <sbkyo88@gmail.com>
2022-10-13 21:51:37 +0900

    develop base shar

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,11 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'auth_service.dart';
+import 'globalWidget.dart';
 import 'main.dart';
+import 'member_service.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -22,113 +26,133 @@ class _HomeState extends State<Home> {
   }
 
   final List<String> entries = <String>['A', 'B', 'C'];
-  final List<int> colorCodes = <int>[600, 500, 100];
+  final List<int> colorCodes = <int>[
+    900,
+    800,
+    700,
+    600,
+    500,
+    400,
+    300,
+    200,
+    100
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        title: Text("Better Coach"),
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.calendar_month),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              print('profile');
-              // 로그인 페이지로 이동
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => LoginPage()),
-              );
-            },
-            icon: Icon(Icons.account_circle),
-          ),
-          IconButton(
-            onPressed: () {
-              _openEndDrawer();
-            },
-            icon: Icon(Icons.menu),
-          ),
-        ],
-      ),
-      endDrawer: SafeArea(
-        child: Container(
-          child: Drawer(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('This is the Drawer'),
-                  ElevatedButton(
-                    onPressed: () {
-                      _closeEndDrawer();
-                    },
-                    child: const Text('Close Drawer'),
-                  )
-                ],
+    final authService = context.read<AuthService>();
+    final user = authService.currentUser()!;
+    return Consumer<MemberService>(
+      builder: (context, memberService, child) {
+        return SafeArea(
+          child: Scaffold(
+            key: _scaffoldKey,
+            appBar: AppBar(
+              title: Text("투데이"),
+              centerTitle: true,
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.calendar_month),
               ),
-            ),
-          ),
-        ),
-      ),
-      body: ListView.separated(
-        itemCount: entries.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 50,
-            color: Colors.amber[colorCodes[index]],
-            child: Center(child: Text('Entry ${entries[index]}')),
-          );
-        },
-        separatorBuilder: ((context, index) => Divider()),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        color: Colors.blue,
-        child: IconTheme(
-          data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 8.0),
-            child: Row(
-              children: [
+              actions: [
                 IconButton(
                   onPressed: () {
-                    print('contacts');
+                    print('profile');
+                    // 로그인 페이지로 이동
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
                   },
-                  icon: Icon(Icons.contacts),
-                  tooltip: 'Client List',
+                  icon: Icon(Icons.account_circle),
                 ),
-                Spacer(),
                 IconButton(
                   onPressed: () {
-                    print('note_add');
+                    _openEndDrawer();
                   },
-                  icon: Icon(Icons.note_add),
-                  tooltip: 'Add Class',
-                ),
-                Spacer(),
-                IconButton(
-                  onPressed: () {
-                    print('search');
-                  },
-                  icon: Icon(Icons.search),
-                  tooltip: 'Search',
+                  icon: Icon(Icons.menu),
                 ),
               ],
             ),
+            endDrawer: Container(
+              child: Drawer(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('This is the Drawer'),
+                      ElevatedButton(
+                        onPressed: () {
+                          _closeEndDrawer();
+                        },
+                        child: const Text('Close Drawer'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // 로그아웃
+                          context.read<AuthService>().signOut();
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (_) => LoginPage()),
+                          );
+                        },
+                        child: const Text('Log Out'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            body: SafeArea(
+              child: FutureBuilder<QuerySnapshot>(
+                  future: memberService.read(user.uid),
+                  builder: (context, snapshot) {
+                    final docs = snapshot.data?.docs ?? []; // 문서들 가져오기
+                    if (docs.isEmpty) {
+                      return Center(child: Text("수업을 준비 중입니다."));
+                    }
+                    return ListView.separated(
+                      itemCount: docs.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final doc = docs[index];
+                        String name = doc.get('name');
+                        String registerDate = doc.get('registerDate');
+                        String phoneNumber = doc.get('phoneNumber');
+                        String registerType = doc.get('registerType');
+                        String goal = doc.get('goal');
+                        String info = doc.get('info');
+                        String note = doc.get('note');
+                        bool isActive = doc.get('isActive');
+                        return InkWell(
+                          onTap: () {
+                            memberService.update(doc.id, !isActive);
+                          },
+                          child: Container(
+                            height: 50,
+                            // color: Colors.amber[colorCodes[index]],
+                            child: Center(
+                              child: Text(
+                                'name : ${name}, goal : ${goal}, info : ${info}, note : ${note}, isActive : ${isActive}',
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      separatorBuilder: ((context, index) => Divider()),
+                    );
+                  }),
+            ),
+            bottomNavigationBar: BaseBottomAppBar(),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                print('floating button');
+              },
+              backgroundColor: Colors.blue,
+              child: const Icon(Icons.add),
+            ),
           ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('floating button');
-        },
-        backgroundColor: Colors.blue,
-        child: const Icon(Icons.add),
-      ),
+        );
+      },
     );
   }
 }
---
+++ lib/memberList.dart
@@ -0,0 +1,174 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'auth_service.dart';
+import 'globalWidget.dart';
+import 'main.dart';
+import 'memberAdd.dart';
+import 'member_service.dart';
+
+class MemberList extends StatefulWidget {
+  const MemberList({super.key});
+
+  @override
+  State<MemberList> createState() => _MemberListState();
+}
+
+class _MemberListState extends State<MemberList> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void _openEndDrawer() {
+    _scaffoldKey.currentState!.openEndDrawer();
+  }
+
+  void _closeEndDrawer() {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final authService = context.read<AuthService>();
+    final user = authService.currentUser()!;
+    return Consumer<MemberService>(
+      builder: (context, memberService, child) {
+        return SafeArea(
+          child: Scaffold(
+            key: _scaffoldKey,
+            appBar: AppBar(
+              title: Text("투데이"),
+              centerTitle: true,
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.calendar_month),
+              ),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    print('profile');
+                    // 로그인 페이지로 이동
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  icon: Icon(Icons.account_circle),
+                ),
+                IconButton(
+                  onPressed: () {
+                    _openEndDrawer();
+                  },
+                  icon: Icon(Icons.menu),
+                ),
+              ],
+            ),
+            endDrawer: Container(
+              child: Drawer(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('This is the Drawer'),
+                      ElevatedButton(
+                        onPressed: () {
+                          _closeEndDrawer();
+                        },
+                        child: const Text('Close Drawer'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          // 로그아웃
+                          context.read<AuthService>().signOut();
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (_) => LoginPage()),
+                          );
+                        },
+                        child: const Text('Log Out'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            body: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      child: FutureBuilder<QuerySnapshot>(
+                        future: memberService.read(user.uid),
+                        builder: (context, snapshot) {
+                          final docs = snapshot.data?.docs ?? []; // 문서들 가져오기
+                          if (docs.isEmpty) {
+                            return Center(child: Text("회원 목록을 준비 중입니다."));
+                          }
+                          return ListView.separated(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            itemCount: docs.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              final doc = docs[index];
+                              String name = doc.get('name');
+                              String registerDate = doc.get('registerDate');
+                              String phoneNumber = doc.get('phoneNumber');
+                              String registerType = doc.get('registerType');
+                              String goal = doc.get('goal');
+                              String info = doc.get('info');
+                              String note = doc.get('note');
+                              bool isActive = doc.get('isActive');
+                              return InkWell(
+                                onTap: () {
+                                  memberService.update(doc.id, !isActive);
+                                },
+                                child: Container(
+                                  height: 50,
+                                  // color: Colors.amber[colorCodes[index]],
+                                  child: Center(
+                                    child: Text(
+                                      'name : ${name}, goal : ${goal}, info : ${info}, note : ${note}, isActive : ${isActive}',
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                            separatorBuilder: ((context, index) => Divider()),
+                          );
+                        },
+                      ),
+                    ),
+                    Divider(height: 1),
+
+                    /// 추가 버튼
+                    ElevatedButton(
+                      child: Text("회원추가", style: TextStyle(fontSize: 21)),
+                      onPressed: () {
+                        print("회원추가");
+                        // create bucket
+                        // 저장하기 성공시 Home로 이동
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (_) => MemberAdd()),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            bottomNavigationBar: BaseBottomAppBar(),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                print('floating button');
+              },
+              backgroundColor: Colors.blue,
+              child: const Icon(Icons.add),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
