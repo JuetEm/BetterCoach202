0614de557b1399df027adde4300d0e0ee23f64b8
정규호 <emotionalmachine@jeong-gyuhoui-MacBookPro.local>
2023-02-17 20:26:51 +0900

    flutter flow ui import

--- lib/app/ui/report.dart
+++ lib/app/ui/report.dart
@@ -8,6 +8,9 @@ import 'package:web_project/memberList_admin.dart';
 
 var fcmToken = null;
 
+TextEditingController pageName = TextEditingController();
+TextEditingController errorContents = TextEditingController();
+
 class Report extends StatefulWidget {
   const Report({super.key});
 
@@ -51,7 +54,108 @@ class _ReportState extends State<Report> {
           ),
       body: Container(
         color: Palette.mainBackground,
-        child: Center(child: Text("리포트 페이지")),
+        child: Padding(
+            padding: EdgeInsetsDirectional.fromSTEB(10, 20, 10, 20),
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(color: Palette.grayFF),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text('오류 및 개선요청', style: TextStyle(fontSize: 16)),
+                  Container(
+                    width: double.infinity,
+                    height: 10,
+                    
+                  ),
+                  Align(
+                    alignment: AlignmentDirectional(-1, 0),
+                    child: Text('문제 페이지',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(color: Palette.gray00, fontSize: 14)),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: Text("dropdown"),
+                  ),
+                  Align(
+                    alignment: AlignmentDirectional(-1, 0),
+                    child: Text('오류 및 개선요청 내용',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(color: Palette.gray00, fontSize: 14)),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(5, 5, 5, 5),
+                    child: TextFormField(
+                      controller: errorContents,
+                      autofocus: true,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        hintText: '오류가 발생한 내용 혹은 개선이 필요한 부분을 알려주세요.',
+                        hintStyle: TextStyle(color: Palette.gray99),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x00000000),
+                            width: 1,
+                          ),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(4.0),
+                            topRight: Radius.circular(4.0),
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x00000000),
+                            width: 1,
+                          ),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(4.0),
+                            topRight: Radius.circular(4.0),
+                          ),
+                        ),
+                        errorBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x00000000),
+                            width: 1,
+                          ),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(4.0),
+                            topRight: Radius.circular(4.0),
+                          ),
+                        ),
+                        focusedErrorBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x00000000),
+                            width: 1,
+                          ),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(4.0),
+                            topRight: Radius.circular(4.0),
+                          ),
+                        ),
+                      ),
+                      style: TextStyle(color: Palette.gray00),
+                      /* validator:
+                          _model.textControllerValidator.asValidator(context), */
+                    ),
+                  ),
+                  Spacer(),
+                  Align(
+                    alignment: AlignmentDirectional(0, -0.05),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(onPressed: (){}, child: Text("취소")),
+                        TextButton(onPressed: (){}, child: Text("제출"))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
       ),
     );
   }
