8085bf10eee60baf8b56714197f00c836e5034af
정규호 <emotionalmachine@jeong-gyuhoui-MacBookPro.local>
2023-02-23 17:42:02 +0900

    sequenceLibrary.dart add

---
+++ lib/app/ui/sequenceLibrary.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class SequenceLibarary extends StatefulWidget {
+  const SequenceLibarary({super.key});
+
+  @override
+  State<SequenceLibarary> createState() => _SequenceLibararyState();
+}
+
+class _SequenceLibararyState extends State<SequenceLibarary> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
