3c3f24c2d2cc9c95eaa68604ea249de544ef8399
Versy <sohnsmith@sonjaehyeong-ui-MacBookPro.local>
2023-02-23 18:31:03 +0900

    up

--- lib/app/ui/sequenceLibrary.dart
+++ lib/app/ui/sequenceLibrary.dart
@@ -12,9 +12,14 @@ class SequenceLibarary extends StatefulWidget {
 class _SequenceLibararyState extends State<SequenceLibarary> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: BaseAppBarMethod(context, "시퀀스 관리", (){Navigator.pop(context);}, null),
-      body: Center(child: Text("시퀀스 관리 화면")),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: BaseAppBarMethod(context, "시퀀스 관리", () {
+          Navigator.pop(context);
+        }, null),
+        body: TabBarView(children: [Text('data'), Text('data'), Text('data')]),
+      ),
     );
   }
 }
