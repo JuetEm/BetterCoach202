00e33e318c8c52957d2f5f0d53f36fe020ff5761
정규호 <emotionalmachine@jeong-gyuhoui-MacBookPro.local>
2023-02-23 18:09:54 +0900

    code merge

--- lib/app/ui/ticketLibrary.dart
+++ lib/app/ui/ticketLibrary.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:web_project/app/binding/ticketLibrary_service.dart';
 import 'package:web_project/app/ui/ticketMake.dart';
 import 'package:web_project/color.dart';
 import 'package:web_project/globalWidget.dart';
 import 'package:web_project/userInfo.dart';
 
 class TicketLibrary extends StatefulWidget {
-  const TicketLibrary(this.userInfo,{super.key});
+  const TicketLibrary(this.userInfo, {super.key});
 
   final UserInfo? userInfo;
 
@@ -17,54 +19,58 @@ class TicketLibrary extends StatefulWidget {
 class _TicketLibraryState extends State<TicketLibrary> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: BaseAppBarMethod(context, "수강권 라이브러리", () {
-            print(
-                "MemberInfo : BaseAppBarMethod : userInfo.bodyAnalyzed : ${widget.userInfo!.selectedBodyAnalyzed}");
-            // print("MemberInfo : BaseAppBarMethod : tmpUserInfo.bodyAnalyzed : ${tmpUserInfo!.selectedBodyAnalyzed}");
-            // Navigator.pop(context,tmpUserInfo);
-            Navigator.pop(context, widget.userInfo);
-          },null),
-      body: Column(children: [
-        // 수강권 추가 버튼
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                  color: Palette.mainBackground,
-                  child: TextButton(
-                    onPressed: () async {
-                      var result = await // 저장하기 성공시 Home로 이동
-                          Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                TicketMake.getUserInfo(widget.userInfo)),
-                      ).then((value) {
-                        print("수강권 추가 result");
-                      });
-                    },
-                    child: Container(
-                      height: 50,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(color: Palette.gray99, width: 2)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "수강권 추가하기",
-                            style:
-                                TextStyle(fontSize: 16, color: Palette.gray66),
-                          ),
-                          Icon(
-                            Icons.add_circle_outline,
-                            color: Palette.gray66,
-                          ),
-                        ],
+    return Consumer<TicketLibraryService>(builder: (context, value, child) {
+      return Scaffold(
+        appBar: BaseAppBarMethod(context, "수강권 라이브러리", () {
+          print(
+              "MemberInfo : BaseAppBarMethod : userInfo.bodyAnalyzed : ${widget.userInfo!.selectedBodyAnalyzed}");
+          // print("MemberInfo : BaseAppBarMethod : tmpUserInfo.bodyAnalyzed : ${tmpUserInfo!.selectedBodyAnalyzed}");
+          // Navigator.pop(context,tmpUserInfo);
+          Navigator.pop(context, widget.userInfo);
+        }, null),
+        body: Column(
+          children: [
+            // 수강권 추가 버튼
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+              color: Palette.mainBackground,
+              child: TextButton(
+                onPressed: () async {
+                  var result = await // 저장하기 성공시 Home로 이동
+                      Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            TicketMake.getUserInfo(widget.userInfo)),
+                  ).then((value) {
+                    print("수강권 추가 result");
+                  });
+                },
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Palette.gray99, width: 2)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "수강권 추가하기",
+                        style: TextStyle(fontSize: 16, color: Palette.gray66),
                       ),
-                    ),
+                      Icon(
+                        Icons.add_circle_outline,
+                        color: Palette.gray66,
+                      ),
+                    ],
                   ),
                 ),
-      ],),
-    );
+              ),
+            ),
+            
+          ],
+        ),
+      );
+    });
   }
 }
